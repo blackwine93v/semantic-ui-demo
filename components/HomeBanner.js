59ab35fc556378abf672723cfc895d3223e4a0b5
Blackwine <daoquocvuong@gmail.com>
2017-03-31 03:33:28 -0700

    Edit home banner

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -1,14 +1,19 @@
 import Head from 'next/head';
-import {Container, Grid, Segment, Header} from 'semantic-ui-react';
+import {Container, Grid, Segment, Header, Icon} from 'semantic-ui-react';
 export default ()=>(
   <div id="homebanner">
     <Head>
       <link rel="stylesheet" href="static/homebanner.css"></link>
     </Head>
     <Grid padded centered columns="10">
-      <Segment basic>
-        <Header inverted as="h1" textAlign="center" content="Semantic UI Demo"></Header>
+      <Segment basic className="segment-content">
+        <Header inverted as="h1" textAlign="center" icon>
+          <Icon name="heart"/>
+          Semantic UI Demo
+          <Header.Subheader content="User Interface is the language of the web"></Header.Subheader>
+        </Header>
       </Segment>
+      <Header className="huge" icon="angle double down"></Header>
     </Grid>
   </div>
 )
